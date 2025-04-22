17667906fa7c532d278beedbbcb5201feb78fd28
syr1234 <gourarisyrine2@gmail.com>
2025-04-22 08:58:46 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyA-L-WFc9wjYU20_fBsACKhXhctSgG2huc",
+authDomain: "hakdak-26489.firebaseapp.com",
+  projectId: "hakdak-26489",
+   storageBucket: "hakdak-26489.firebasestorage.app",
+  messagingSenderId: "549900038178",
+  appId: "1:549900038178:web:1fa052327e635f725c1a7b"
+};
